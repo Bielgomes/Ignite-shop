79cd1f1c21d5245cb3323d1613e7465e8de97bb4
Gabriel Gomes da Silva <72311029+Bielgomes@users.noreply.github.com>
2022-10-19 17:18:04 -0300

    Adding SSG render

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,72 @@
-import { styled } from "../styles"
+import { GetStaticProps } from "next"
+import Image from "next/future/image"
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
+import { useKeenSlider } from 'keen-slider/react'
+import { stripe } from "../lib/stripe"
 
-  span: {
-    fontWeight: 'bold',
-  },
+import { HomeContainer, Product } from "../styles/pages/home"
 
-  '&:hover': {
-    filter: 'brightness(0.9)'
-  }
-})
+import 'keen-slider/keen-slider.min.css'
+import Stripe from "stripe"
 
-export default function Home() {
+interface HomeProps {
+  products: {
+    id: string
+    name: string
+    imageUrl: number
+    price: number
+  }[]
+}
+
+export default function Home({ products }) {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-    <Button>
-      <span>Teste</span>
-      Enviar
-    </Button>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      {products.map(product => {
+        return (
+          <Product key={product.id} className="keen-slider__slide">
+            <Image src={product.imageUrl} width={520} height={480} alt="" />
+
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        )
+      })}
+    </HomeContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
+
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(price.unit_amount / 100)
+    }
+  })
+
+  return {
+    props: {
+      products
+    },
+    revalidate: 60 * 60 * 2 // 2 Horas
+  }
 }
